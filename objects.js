e545d1804b7699dfe937457c392903f5e21ed638
Benjamin Barimah-Sarpong <benjaminbarimahsarpong@gmail.com>
2025-05-25 23:20:52 +0000

    added new lines to these files

---
+++ objects.js
@@ -0,0 +1,13 @@
+let ninjas = ['shaun', 'ryu', 'Owusua'];
+
+console.log(ninjas);
+ninjas[1] = 'benjamin'; //update the second element in the array
+console.log(ninjas[1]);
+
+let ages = [15, 20, 30, 35];
+console.log(ages[2]);
+console.log(ages.length);
+console.log(ages[ages.length -1]);
+
+let dogs = ['pitbull', 'bulldog', 'german shepherd', 'poddle' ];
+console.log(dogs[3].toUpperCase());
--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,5 @@
-let age = 26;
-let year = 2025;
+//let age = 26;
+//let year = 2025;
 //log things to console;
 /*
 console.log(age);
@@ -10,6 +10,28 @@ console.log(age,year);
 const points = 500;
 console.log(points);*/
 
-var score = 100;
-score =75;
-console.log(score);
+//var score = 100;
+//score =75;
+//console.log(score);
+
+
+const title = 'Best roads of 2025';
+const author = 'Benjamin Barimah-Sarpong';
+const heart = 100;
+
+//concatenation way
+let result8 = 'The blog called ' + title + ' by ' + author + ' has ' + heart + ' likes';
+console.log(result8.toUpperCase());
+
+//template string way //template string is inserted using back ticks ``
+let result9 = `The blog called ${title} by ${author} has ${heart} likes`;
+console.log(result9);
+
+//crating HTML templates
+let html = `
+ <h2>${title}</h2>
+ <p>By ${author}</p>
+ <span>This blog has ${heart} likes</span>
+`;
+
+console.log(html)
