83792abf5f44dfe44bab799c7f8be7df9c69623f
Benjamin Barimah-Sarpong <benjaminbarimahsarpong@gmail.com>
2025-05-25 22:04:14 +0000

    made changes to all files

--- numbers.js
+++ numbers.js
@@ -16,8 +16,27 @@
 //console.log(result6);
 
 let likes = 10;
-likes ++; //likes = likes + 1
-console.log(likes);
+//likes ++; //likes = likes + 1
+//console.log(likes);
 
-likes --; //likes = likes - 1
-console.log(likes);
+//likes --; //likes = likes - 1
+//console.log(likes);
+
+//likes += 5;
+//console.log(likes);
+//likes -= 2;
+//console.log(likes);
+
+//likes *= 2;
+//console.log(likes);
+
+//Nan //not a Number
+console.log(5 / 'hello'); // NaN
+console.log(5 * 'hello');
+
+//number concatenation;
+let score =100;
+let result7 = 'The score is ' + score + ' points ';
+console.log(result7);
+
+console.log('The blog has ' + likes + ' likes');
