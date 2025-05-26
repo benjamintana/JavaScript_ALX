c1e1f9f4a89fe3bc2d162b4c56dcf012969b80de
Benjamin Barimah-Sarpong <benjaminbarimahsarpong@gmail.com>
2025-05-26 12:25:07 +0000

    modified object.js and indx.html

--- objects.js
+++ objects.js
@@ -1,13 +1,29 @@
 let ninjas = ['shaun', 'ryu', 'Owusua'];
 
-console.log(ninjas);
-ninjas[1] = 'benjamin'; //update the second element in the array
-console.log(ninjas[1]);
+//console.log(ninjas);
+//ninjas[1] = 'benjamin'; //update the second element in the array
+//console.log(ninjas[1]);
 
 let ages = [15, 20, 30, 35];
-console.log(ages[2]);
-console.log(ages.length);
-console.log(ages[ages.length -1]);
+//console.log(ages[2]);
+//console.log(ages.length);
+//console.log(ages[ages.length -1]);
 
 let dogs = ['pitbull', 'bulldog', 'german shepherd', 'poddle' ];
-console.log(dogs[3].toUpperCase());
+//console.log(dogs[3].toUpperCase());
+
+random = ['shaun', 'ryu', 'Owusua', 20, 30, 40];
+//console.log(random);
+console.log(random.length);
+
+//array methods
+ let persons = ['kezia ', ' Benjamin ', ' Nana'];
+//console.log(persons.join(',')); // joins the array elements into a string with 'Akosua' as a separator]
+//console.log(persons.push('Akosua')); // adds 'Akosua' to the end of the array and returns the new length of the array
+//console.log(persons); // logs the updated array with 'Akosua' added
+//console.log(persons.indexOf(' Benjamin ')); // finds the index of 'Benjamin' in the array and returns it
+
+//console.log(persons.concat('Akosua')); // concatenates 'Akosua' to the end of the array and returns a new array
+//console.log(persons.pop()); // removes the last element from the array and returns it
+ //persons = persons.pop()
+//console.log(persons);
