214b6d0d630ef53d816055c155d3cdce61d0d807
Benjamin Barimah-Sarpong <benjaminbarimahsarpong@gmail.com>
2025-05-26 12:23:26 +0000

    added new files

---
+++ null and undefined.js
@@ -0,0 +1,58 @@
+let age;
+//console.log(age, age + 3, `the age is ${age}`); //undefined
+
+age = null
+//console.log(age, age + 3, `the age is ${age}`);
+
+//booleans and comparisons
+//console.log(true, false);
+//console.log("true", "false") //these are not the same
+
+let email = "benjamintana0@gmail.com";
+let people = ['Benjamin ', ' Kezia']
+//let result = email.includes('q');
+ //console.log(result);
+//console.log(people.includes('Tana'));
+
+//comparison operators
+//console.log(people.length >= 5);
+//console.log(people[0] != 'Benjamin '); //true
+
+age = 26
+//console.log(age <= 26); //true
+//console.log(age !== 26); //false
+
+//console.log(age + 25 > 40); //true
+
+//const name = 'Owusua'
+
+//console.log(name == 'owusua'); //false
+//console.log(name == 'Owusua'); //true
+//console.log(name > 'Owusua'); //false
+
+//strict comparisons( different types cannot be equal)
+//console.log(age == '26' ); //true')
+ //console.log(age === '26'); //false
+
+ //type conversion
+ //let score = '100';
+
+//console.log(score + 1); //1001 (string concatenation
+
+ //score = Number(score); // converts string to number
+//console.log(score + 1); //101 (number addition)
+
+//console.log(typeof score);
+
+ let result = Number('hello'); // NaN (not a number)
+ //console.log(result); // NaN
+
+ let string = String(100);
+    //console.log(string); // "NaN" (string representation of NaN)
+    //console.log(string, typeof string);
+
+    let percentage =(100);
+    console.log(percentage)
+
+    console.log(Boolean(0)); // false
+    console.log(percentage === '100', typeof percentage);
---
+++ practice.js
@@ -0,0 +1,35 @@
+let name = 'Benjamin Barimah-Sarpong';
+let age = 26;
+let isStudent = true;
+let address = {city: 'Accra', 
+    country: 'Ghana'};
+let hobbies = ['playing games ', 'learning', ' talking to friends'];
+
+let favPet;
+
+let favMovie = null
+
+console.log(name, typeof name);
+console.log(age, typeof age);
+console.log(isStudent, typeof isStudent);
+console.log(address, typeof address);
+console.log(hobbies, typeof hobbies);
+console.log(favPet, typeof favPet);
+console.log(favMovie, typeof favMovie);
+
+//prompt for name and age:
+
+let userName = prompt("What is your name?");
+let userAge = prompt("What is your age?");
+
+userAge = Number(userAge);
+
+//calculate year of birth
+
+let currentYear = new Date().getFullYear();
+
+let yearOfBirth = currentYear - userAge;
+console.log(yearOfBirth)
+
+//alert(`Hello, your name is ${userName} and you are ${userAge}, that means you were born in ${yearOfBirth}`);
+alert('Hello, your name is ' + userName + ' and you are ' + userAge + ', that means you were born in ' + yearOfBirth);
